509a053606c727f89d7b6af05bc6d0f8136e1ac9
sobhant <sobhant@mindfiresolutions.com>
2019-12-12 18:07:24 +0530

    Base Controller Setup for QBD Resources

--- src/AppBundle/Controller/API/QBDIntegration/QBDResourcesController.php
+++ src/AppBundle/Controller/API/QBDIntegration/QBDResourcesController.php
@@ -8,22 +8,49 @@
 
 namespace AppBundle\Controller\API\QBDIntegration;
 
+use AppBundle\Constants\GeneralConstants;
+use AppBundle\QBDHelpers\Applications\QBDResourcesService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class AddCustomersController
+ * Class QBDResourcesController
  * @package AppBundle\Controller\API\QBDIntegration
  */
-class AddCustomersController extends Controller
+class QBDResourcesController extends Controller
 {
     /**
      * @Route("qbdresources/sync")
      * @param Request $request
+     * @return Response
      */
-    public function AddCustomersRequest(Request $request)
+    public function QBDResourcesRequest(Request $request)
     {
+        $logger = $this->container->get(GeneralConstants::MONOLOG_EXCEPTION);
+        try {
+//        $content = simplexml_load_string($request->getContent())->xpath('soap:Body');
+            $qbdResource = new QBDResourcesService([
+                'login' => 'VRS5d9dd0b9237e6',
+                'password' => '1',
+                'iterator' => null,
+                'wsdlPath' => $this->container->getParameter('wsdlpath')
+            ]);
 
+            $server = new \SoapServer($this->container->getParameter('wsdlpath'),array('cache_wsdl' => WSDL_CACHE_NONE));
+            $server->setObject($qbdResource);
+
+            $response = new Response();
+            $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+            ob_start();
+            $server->handle();
+            $response->setContent(ob_get_clean());
+
+            return $response;
+        } catch (\Exception $exception) {
+            throw $exception;
+            $logger->error($exception->getMessage());
+        }
     }
 }
