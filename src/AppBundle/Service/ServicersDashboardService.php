53756a81edeca0c34acb8ed0de9bfcbc162dfcc7
sobhanthakur <sobhant@mindfiresolutions.com>
2020-03-03 14:55:41 +0530

    Change Authenticate API to Get Request. Add filters to GetTasks API

--- src/AppBundle/Service/ServicersDashboardService.php
+++ src/AppBundle/Service/ServicersDashboardService.php
@@ -22,14 +22,40 @@ class ServicersDashboardService extends BaseService
             $tasks = $this->entityManager->getRepository('AppBundle:Tasks')->FetchTasksForDashboard($servicerID, $servicers);
 
             for ($i=0; $i<count($tasks); $i++) {
-                // Task Estimates Response
+                // Initialize local variables
                 $taskEstimates = null;
+                $guestDetails = null;
+                $acceptDecline = null;
+                $expand = null;
+                $startTask = null;
+
+                // Show AcceptDecline
+                if($servicers[0]['RequestAcceptTasks'] && !$tasks[$i]['AcceptedDate']) {
+                    $acceptDecline = 1;
+                }
+                $response[$i]['AcceptDecline'] = $acceptDecline;
+
+                // Show or hide expand
+                if(!$acceptDecline && (!$servicers[0]['TimeTracking'] || $servicers[0]['TimeTracking'] === 0)) {
+                    $expand = 1;
+                }
+                $response[$i]['Expand'] = $expand;
+
+                // Show or hide Start Task
+                if(!$acceptDecline && (int)$servicers[0]['TimeTracking'] === 1) {
+                    $startTask = 1;
+                }
+                $response[$i]['StartTask'] = $startTask;
+
+                // Task Estimates Response
+
                 if($servicers[0]['ShowTaskEstimates']) {
-                    $response[$i]['TaskEstimates'] = array(
+                    $taskEstimates = array(
                         'Min' => $tasks[$i]['Min'],
                         'Max' => $tasks[$i]['Max']
                     );
                 }
+                $response[$i]['TaskEstimates'] = $taskEstimates;
 
                 // Assigned Date
                 $response[$i]['AssignedDate'] = $tasks[$i]['AssignedDate'];
@@ -47,16 +73,15 @@ class ServicersDashboardService extends BaseService
                 );
 
                 // Guest Details
-                if(!$servicers[0]['IncludeGuestNumbers'] && !$servicers[0]['IncludeGuestEmailPhone'] && !$servicers[0]['IncludeGuestName']) {
-                    $response[$i]['GuestDetails'] = null;
-                } else {
-                    $response[$i]['GuestDetails'] = array(
+                if ($servicers[0]['IncludeGuestNumbers'] && $servicers[0]['IncludeGuestEmailPhone'] && $servicers[0]['IncludeGuestName']) {
+                    $guestDetails = array(
                         'Name' => $tasks[$i]['Name'],
                         'Email' => $tasks[$i]['Email'],
                         'Phone' => $tasks[$i]['Phone'],
                         'Number' => $tasks[$i]['Number']
                     );
                 }
+                $response[$i]['GuestDetails'] = $guestDetails;
             }
 
             return array('Tasks' => $response);
