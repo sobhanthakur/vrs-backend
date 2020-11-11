e7f4cf12e13c31152c54433659da5af168b5fec3
sobhanthakur <sobhant@mindfiresolutions.com>
2020-11-11 16:55:30 +0530

    Restrict admin mails

--- src/AppBundle/EventListener/ExceptionListener.php
+++ src/AppBundle/EventListener/ExceptionListener.php
@@ -125,7 +125,18 @@ class ExceptionListener extends BaseService
                 }
             }
             $content['UserInfo'] = $authPayload;
-            $this->serviceContainer->get('vrscheduler.mail_service')->SendMailFunction($content);
+
+            // Restrict Admin Mail
+            // Remove this later
+            $sendToDev = null;
+            if ($request->attributes->get('_route') === 'vrs_pwa_unscheduled_tasks_complete' ||
+                $request->attributes->get('_route') === 'vrs_pwa_upload_image_post' ||
+                $request->attributes->get('_route') === 'vrs_pwa_manage_submit' ||
+                $request->attributes->get('_route') === 'vrs_pwa_issue_post'
+            ) {
+                $sendToDev = 1;
+            }
+            $this->serviceContainer->get('vrscheduler.mail_service')->SendMailFunction($content,$sendToDev);
         }
 
         $event->setResponse($response);
