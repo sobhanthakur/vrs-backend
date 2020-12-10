6f1c52a6a46f7d1fcbb139c0ede9580d297acc42
sobhanthakur <sobhant@mindfiresolutions.com>
2020-12-11 00:43:26 +0530

    Modify Piece Pay Logic. Fetch PayType from TasksToServicers Table

--- src/AppBundle/Service/ServicersDashboardService.php
+++ src/AppBundle/Service/ServicersDashboardService.php
@@ -172,7 +172,18 @@ class ServicersDashboardService extends BaseService
                 ) {
                     $quickChangeAbbreviation = trim($servicers[0]['QuickChangeAbbreviation']);
                 }
-                $servicers[0]['ShowPiecePayAmountsOnEmployeeDashboards'] ? $piecePay = ((int)$tasks[$i]['PayType'] === 2 ? $tasks[$i]['PiecePay'] * $servicers[0]['PayRate'] : $tasks[$i]['PiecePay']) : $piecePay = null;
+
+                $piecePay = null;
+
+                if ((int)$servicers[0]['ShowPiecePayAmountsOnEmployeeDashboards'] === 1) {
+                    if ((int)$tasks[$i]['PayType'] === 1) {
+                        $piecePay = $tasks[$i]['PiecePay'];
+                    }
+
+                    if ((int)$tasks[$i]['PayType'] === 2) {
+                        $piecePay = $tasks[$i]['PiecePay'] * $servicers[0]['PayRate'];
+                    }
+                }
 
                 // Check Scheduling Notes
                 $schedulingNote = null;
