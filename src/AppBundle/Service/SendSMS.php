2d3c2f9fdd92de1f8e1499df8e7b5cdcccfd1e71
sobhanthakur <sobhant@mindfiresolutions.com>
2021-02-01 12:40:01 +0530

    Add AWS SMS Response into the HTTP Error email

--- src/AppBundle/Service/SendSMS.php
+++ src/AppBundle/Service/SendSMS.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Service;
 
 use AppBundle\Constants\ErrorConstants;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
@@ -20,13 +21,14 @@ use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 class SendSMS extends BaseService
 {
     /**
-     * @param $content
+     * @param Request $request
      * @return array
      */
-    public function SendSMS($content)
+    public function SendSMS($request)
     {
         $response = [];
         $aws = $this->serviceContainer->getParameter('aws')['sns'];
+        $content = json_decode($request->getContent(),true);
         try {
             $params = array(
                 'credentials' => ['key' => $aws['key'], 'secret' => $aws['secret']],
@@ -63,9 +65,24 @@ class SendSMS extends BaseService
         } catch (HttpException $exception) {
             throw $exception;
         } catch (\Exception $exception) {
+            $str = $exception->getMessage();
+            $from = '<Message>';
+            $sub = substr($str, strpos($str,$from),strlen($str));
+            $error = $this->get_string_between($sub,"<Message>","</Message>");
+            $request->attributes->set('SMS',$error);
+
             $this->logger->error('Failed Sending SMS due to : ' .
                 $exception->getMessage());
             throw new HttpException(500, ErrorConstants::INTERNAL_ERR);
         }
+    }
+
+    function get_string_between($string, $start, $end){
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
     }
 }
