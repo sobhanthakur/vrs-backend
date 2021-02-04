2c3bad0c33d8dc22ef4baae6672163bc23caa79a
sobhanthakur <sobhant@mindfiresolutions.com>
2021-02-04 20:05:48 +0530

    Revert Changes

--- src/AppBundle/Service/ServicersDashboardService.php
+++ src/AppBundle/Service/ServicersDashboardService.php
@@ -33,7 +33,6 @@ class ServicersDashboardService extends BaseService
     public function GetTasks($servicerID)
     {
         try {
-            throw new HttpException(500, ErrorConstants::INTERNAL_ERR);
             $response = [];
             $servicers = $this->entityManager->getRepository('AppBundle:Servicers')->ServicerDashboardRestrictions((int)$servicerID);
             $tasks = $this->entityManager->getRepository('AppBundle:Tasks')->FetchTasksForDashboard((int)$servicerID, $servicers);
