c674695775c06d47d309ef54ea3afcd4d0f8b836
sobhanthakur <sobhant@mindfiresolutions.com>
2021-03-16 12:20:52 +0530

    Refactor Response listeners

--- src/AppBundle/EventListener/ResponseListener.php
+++ src/AppBundle/EventListener/ResponseListener.php
@@ -8,7 +8,6 @@
 
 namespace AppBundle\EventListener;
 
-use AppBundle\Constants\ApiRoutes;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use AppBundle\Service\BaseService;
@@ -44,29 +43,33 @@ class ResponseListener extends BaseService
             $response->setStatusCode(201);
         }
 
+        // Make HTTP Status Code to 200 for QB auth API
+        if($request->attributes->get('_route') === 'oauth_validate_post' &&
+            $response->getStatusCode() === 401
+        ) {
+            $response->setStatusCode(200);
+        }
+
         //Enable Headers for CORS
         $response->headers->set('Access-Control-Allow-Origin','*');
-        $response->headers->set('Access-Control-Allow-Headers','X-Requested-With, Cache-Control, Content-Type, Authorization, Accept-Language,Content-Language,Last-Event-ID,X-HTTP-Method-Override, Offline');
+        $response->headers->set('Access-Control-Allow-Headers','X-Requested-With, Cache-Control, Content-Type, Authorization, Accept-Language,Content-Language,Last-Event-ID,X-HTTP-Method-Override');
         $response->headers->set('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Credentials', true);
 
-        // Stop Response logging temporarily
-        // uncomment this in future.
-
         $responseContent = $response->getContent();
 
-        $route = $request->attributes->get('_route');
-
-        if (!in_array($route,ApiRoutes::RESTRICT_RESPONSE_LOGS) && $request->getMethod() !== 'GET' && $request->getMethod() !== 'OPTIONS') {
-            $this->apiLogger->debug('API Request/Response',
-                array_merge($request->headers->all(),
-                    [
-                        'request_content' => $request->getContent(),
-                        'response_content' => $responseContent,
-                        'response_status_code' => $response->getStatusCode()
-                    ]
-                )
-            );
-        }
+        $this->apiLogger->debug('API Request/Response',
+            array_merge($request->headers->all(),
+                [
+                    'host' => $request->getSchemeAndHttpHost(),
+                    'uri' => $request->getRequestUri(),
+//                    'headers' => $request->headers->all(),
+                    'method' => $request->getMethod(),
+                    'request_content' => $request->getContent(),
+                    'response_content' => $responseContent,
+                    'response_status_code' => $response->getStatusCode()
+                ]
+            )
+        );
     }
 }
